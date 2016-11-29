a713a66d1e4efb4b81d03e90dd70bdbde2ba60ba
krsunyoung <krsunyoung@naver.com>
2016-11-29 11:49:07 +0900

    1129 html string ui에 적용.  이메일 password

--- app/src/main/java/com/bit2016/loginactivity/MainActivity.java
+++ app/src/main/java/com/bit2016/loginactivity/MainActivity.java
@@ -2,6 +2,9 @@ package com.bit2016.loginactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TextView textView = (TextView)findViewById( R.id.tv_signup );
-//        textView.setMovementMethod( LinkMovementMethod.getInstance() );
-//        textView.setText( Html.fromHtml( "<a href=http://www.hellomyoffice.com>Hello My Office 가입하기<;/a>" ));
+        //html String UI에 적용하기
+        TextView textView = (TextView)findViewById( R.id.tv_signup );
+        textView.setMovementMethod( LinkMovementMethod.getInstance() );
+        textView.setText( Html.fromHtml( getString(R.string.login_url) )); //R.string.login_url 이렇게 못쓴다. r은 integer를 말하기 때문에
     }
 }
